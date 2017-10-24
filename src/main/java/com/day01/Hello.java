403e2da53e966763ccc4f848353d76507073b253
K, Pavithra <Pavithra_K@intuit.com>
2017-10-24 15:10:35 +0530

    Intial commit

---
+++ src/main/java/com/day01/Hello.java
@@ -0,0 +1,18 @@
+package com.day01;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by pk4 on 10/24/17.
+ */
+@RestController
+public class Hello {
+
+    @RequestMapping(value="/hello")
+    public String sayHello(@RequestParam(name="name") String name)
+    {
+        return "Hello " + name;
+    }
+}
